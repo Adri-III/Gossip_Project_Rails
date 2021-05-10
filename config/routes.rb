af9990287938671f0199af8b1f9fbeeb1a0e1ce6
Adri-III <trrs.adrien@gmail.com>
2021-05-10 23:16:58 +0200

    Projet Validant - Ruby on Rails: Gossip_Project

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  root to: 'pages#home'
+
+  get '/welcome', to: 'pages#welcome'
+
+  get '/team', to: 'pages#team'
+
+  get '/contact', to: 'pages#contact'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
